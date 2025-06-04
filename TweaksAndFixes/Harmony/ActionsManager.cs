2ef454945adf8e0780bd54a5951bd852ddf6fb96
DukeDagor <dagorzynski@gmail.com>
2025-06-04 12:21:54 -0400

    Rebase

---
+++ TweaksAndFixes/Harmony/ActionsManager.cs
@@ -0,0 +1,22 @@
+﻿using MelonLoader;
+using HarmonyLib;
+using UnityEngine;
+using Il2Cpp;
+using System.Collections.Generic;
+using Il2CppInterop.Runtime.InteropTypes.Arrays;
+using static Il2Cpp.ActionsManager;
+using System.Runtime.InteropServices;
+
+namespace TweaksAndFixes
+{
+    [HarmonyPatch(typeof(CampaignMap))]
+    internal class Patch_ActionsManager
+    {
+        // [HarmonyPatch(nameof(ActionsManager.ReportAction))]
+        // [HarmonyPrefix]
+        // internal static void Prefix_ReportAction(bool success, ActionType action, Player player, Player choosenPlayer, bool isAi, params int[] param)
+        // {
+        //     Melon<TweaksAndFixes>.Logger.Msg("Report Action: " + player.Name(false) + " " + (success ? "SUCCEDED" : "FAILED") + " to " + action + " for " + choosenPlayer.Name(false));
+        // }
+    }
+}
