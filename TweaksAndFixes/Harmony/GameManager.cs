1d1a0145b52209635dc64d321cbe140583acaa4d
DukeDagor <dagorzynski@gmail.com>
2025-08-14 19:06:06 -0400

    Implement simple state machine for the dockyard
    Track new, existing, viewing, and shared with temporary debug comments.

--- TweaksAndFixes/Harmony/GameManager.cs
+++ TweaksAndFixes/Harmony/GameManager.cs
@@ -5,6 +5,8 @@ using HarmonyLib;
 using UnityEngine;
 using Il2Cpp;
 using UnityEngine.UI;
+using static Il2Cpp.GameManager;
+using Il2CppSystem.Globalization;
 
 namespace TweaksAndFixes
 {
@@ -25,6 +27,80 @@ namespace TweaksAndFixes
         internal static void Postfix_RefreshSharedDesign()
         {
             _IsRefreshSharedDesign = false;
+        }
+
+        public enum SubGameState
+        {
+            InSharedDesigner,
+            InConstructorNew, // New ship
+            InConstructorExisting, // Refitting or modifying custom battle ship
+            InConstructorViewMode, // Can't edit, only view
+            Other,
+        }
+
+        public static SubGameState CurrentSubGameState = SubGameState.Other;
+
+        [HarmonyPrefix]
+        [HarmonyPatch(nameof(GameManager.ToSharedDesignsConstructor))]
+        internal static void Prefix_ToSharedDesignsConstructor(int year, PlayerData nation, bool forceCreateNew)
+        {
+            Melon<TweaksAndFixes>.Logger.Msg($"ToSharedDesignsConstructor: year {year}, nation {nation.nameUi}, forceCreateNew {forceCreateNew}");
+            CurrentSubGameState = SubGameState.InSharedDesigner;
+            Patch_Ui.NeedsConstructionListsClear = true;
+            Patch_Ui.NeedsForcedUpdate = true;
+        }
+
+        [HarmonyPrefix]
+        [HarmonyPatch(nameof(GameManager.ToConstructor))]
+        internal static void Prefix_ToConstructor(bool newShip, Ship viewShip, bool allowEdit, IEnumerable<Ship> allowEditMany, ShipType shipTypeNew, bool needCleanup, Player newPlayer)
+        {
+            Melon<TweaksAndFixes>.Logger.Msg(
+                $"ToConstructor: " +
+                $"bool newShip {newShip}, " +
+                $"Ship viewShip {(viewShip != null ? viewShip.Name(false, false) : "NULL")}, " +
+                $"bool allowEdit {allowEdit}, " +
+                $"IEnumerable<Ship> allowEditMany {(allowEditMany != null ? new List<Ship>(allowEditMany).Count : "NULL")}, " +
+                $"ShipType shipTypeNew, {(shipTypeNew != null ? shipTypeNew.nameUi : "NULL")} " +
+                $"bool needCleanup, {needCleanup} " +
+                $"Player newPlayer {(newPlayer != null ? newPlayer.Name(false) : "NULL")} "
+            );
+
+            if (newShip && allowEdit && viewShip == null)
+            {
+                Melon<TweaksAndFixes>.Logger.Msg($"  Regular constructor with new desgin");
+                CurrentSubGameState = SubGameState.InConstructorNew;
+            }
+            else if (!newShip && allowEdit && viewShip != null)
+            {
+                Melon<TweaksAndFixes>.Logger.Msg($"  Refit mode or existing design: {viewShip.Name(false, false)}");
+                CurrentSubGameState = SubGameState.InConstructorExisting;
+            }
+            else if (!newShip && !allowEdit && viewShip != null)
+            {
+                Melon<TweaksAndFixes>.Logger.Msg($"  View mode for: {viewShip.Name(false, false)}");
+                CurrentSubGameState = SubGameState.InConstructorViewMode;
+            }
+            else
+            {
+                Melon<TweaksAndFixes>.Logger.Msg($"  Unknown state!");
+            }
+
+            Patch_Ui.NeedsForcedUpdate = true;
+            Patch_Ui.NeedsConstructionListsClear = true;
+        }
+
+        [HarmonyPrefix]
+        [HarmonyPatch(nameof(GameManager.UpdateLoadingConstructor))]
+        internal static void Prefix_UpdateLoadingConstructor(bool needCleanup, Il2CppSystem.Action onDone)
+        {
+            Melon<TweaksAndFixes>.Logger.Msg($"UpdateLoadingConstructor: bool needCleanup {needCleanup}, Il2CppSystem.Action onDone {(onDone != null ? onDone.method_ptr : "NULL")}");
+        }
+
+        [HarmonyPrefix]
+        [HarmonyPatch(nameof(GameManager.ChangeState))]
+        internal static void Prefix_ChangeState(GameState newState, bool raiseEnterStateEvents)
+        {
+            Melon<TweaksAndFixes>.Logger.Msg($"ChangeState: GameState newState {newState}, bool raiseEnterStateEvents {raiseEnterStateEvents}");
         }
 
         [HarmonyPrefix]
