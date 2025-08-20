8f778b1364e33c01af28d93217c4091cbd872793
DukeDagor <dagorzynski@gmail.com>
2025-08-20 14:15:55 -0400

    Interface with base-game settings to update new setting

---
+++ TweaksAndFixes/Harmony/Settings.cs
@@ -0,0 +1,26 @@
+﻿using MelonLoader;
+using HarmonyLib;
+using UnityEngine;
+using Il2Cpp;
+using System.Collections.Generic;
+
+namespace TweaksAndFixes
+{
+    [HarmonyPatch(typeof(Settings))]
+    internal class Patch_Settings
+    {
+        [HarmonyPatch(nameof(Settings.ApplyOnInit))]
+        [HarmonyPostfix]
+        internal static void Postix_Load()
+        {
+            UiM.LoadSettings();
+        }
+
+        [HarmonyPatch(nameof(Settings.Save))]
+        [HarmonyPostfix]
+        internal static void Postix_Save()
+        {
+            UiM.SaveSettings();
+        }
+    }
+}
