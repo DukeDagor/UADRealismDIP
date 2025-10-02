e5b3b0655440f7122e640d422069b857b7b0f054
DukeDagor <dagorzynski@gmail.com>
2025-10-02 12:32:34 -0400

    Implement player GDP modifier
    Create system for modifing player GDP at the end of their turn.

--- TweaksAndFixes/Harmony/Player.cs
+++ TweaksAndFixes/Harmony/Player.cs
@@ -23,6 +23,67 @@ namespace TweaksAndFixes
             return true;
         }
 
+        private static Dictionary<Player, float> GDPMultiplier = new();
+        private static Dictionary<Player, float> GDPMultiplierNext = new();
+        private static int TurnForGDPMultipler = -1;
+
+        public static void RequestChangePlayerGDP(Player player, float multiplier)
+        {
+            if (TurnForGDPMultipler == -1) TurnForGDPMultipler = CampaignController.Instance.CurrentDate.turn;
+
+            if (TurnForGDPMultipler < CampaignController.Instance.CurrentDate.turn)
+            {
+                if (!GDPMultiplierNext.ContainsKey(player)) GDPMultiplierNext[player] = 0;
+
+                GDPMultiplierNext[player] += multiplier;
+
+                // Melon<TweaksAndFixes>.Logger.Msg($"Next: {player.Name(false)} - {multiplier} -> {GDPMultiplierNext[player]} | {CampaignController.Instance.CurrentDate.turn}");
+            }
+            else
+            {
+                if (!GDPMultiplier.ContainsKey(player)) GDPMultiplier[player] = 0;
+
+                GDPMultiplier[player] += multiplier;
+
+                // Melon<TweaksAndFixes>.Logger.Msg($"Curr: {player.Name(false)} - {multiplier} -> {GDPMultiplier[player]} | {CampaignController.Instance.CurrentDate.turn}");
+            }
+
+        }
+
+        public static void ResetChangePlayerGDP()
+        {
+            GDPMultiplier.Clear();
+
+            TurnForGDPMultipler = CampaignController.Instance.CurrentDate.turn;
+
+            // Melon<TweaksAndFixes>.Logger.Msg($"Reset GDP Multiplier: {CampaignController.Instance.CurrentDate.turn}");
+
+            foreach (var pair in GDPMultiplierNext)
+            {
+                // Melon<TweaksAndFixes>.Logger.Msg($"  Transfer multiplier: {pair.Key.Name(false)} : {pair.Value}");
+                GDPMultiplier[pair.Key] = pair.Value;
+            }
+
+            GDPMultiplierNext.Clear();
+        }
+
+        [HarmonyPatch(nameof(Player.WealthGrowthEffective))]
+        [HarmonyPostfix]
+        internal static void Postfix_WealthGrowthEffective(Player __instance, ref float __result)
+        {
+            if (GDPMultiplier.ContainsKey(__instance))
+            {
+                Melon<TweaksAndFixes>.Logger.Msg($"Apply event GDP% modifier to {__instance.Name(false)} : Base {__result} | Modifier {GDPMultiplier[__instance]}");
+            
+                __result += GDPMultiplier[__instance];
+            }
+
+            // if (CampaignController.Instance.CurrentDate.turn > TurnForGDPMultipler)
+            // {
+            //     ResetChangePlayerGDP();
+            // }
+        }
+
         // These are done as postfix so that TotalPopulation gets set properly
         [HarmonyPatch(nameof(Player.InitCrewPool))]
         [HarmonyPostfix]
