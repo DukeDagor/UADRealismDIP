d8c6c7324b2d8aefd0f17dd8372f5e50ead01d4d
DukeDagor <dagorzynski@gmail.com>
2026-05-14 08:35:24 -0400

    Add function for forcing cloned ship to have updated techs

--- TweaksAndFixes/Harmony/PlayerController.cs
+++ TweaksAndFixes/Harmony/PlayerController.cs
@@ -6,6 +6,20 @@ namespace TweaksAndFixes
     [HarmonyPatch(typeof(PlayerController))]
     internal class Patch_PlayerController
     {
+        public static bool updateTechsForNextClonedShips = false;
+
+        [HarmonyPatch(nameof(PlayerController.CloneShipRaw))]
+        [HarmonyPrefix]
+        internal static void Prefix_CloneShipRaw(ref bool newTechnologies)
+        {
+            if (GameManager.IsConstructor && GameManager.IsCampaign && updateTechsForNextClonedShips)
+            {
+                // Melon<TweaksAndFixes>.Logger.Msg($"Updating techs for cloned ship.");
+                newTechnologies = true;
+                updateTechsForNextClonedShips = false;
+            }
+        }
+
         [HarmonyPatch(nameof(PlayerController.CloneShipRaw))]
         [HarmonyPostfix]
         internal static void Postfix_CloneShipRaw(Ship from, ref Ship __result)
