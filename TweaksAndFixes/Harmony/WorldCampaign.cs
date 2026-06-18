ffaeee70aa2c72ff248aea7671f5edb94610e31b
DukeDagor <dagorzynski@gmail.com>
2026-06-18 16:54:13 -0400

    When vingettes are disabled, they are replaced by a hard black boarder.

--- TweaksAndFixes/Harmony/WorldCampaign.cs
+++ TweaksAndFixes/Harmony/WorldCampaign.cs
@@ -20,9 +20,28 @@ namespace TweaksAndFixes
                 // Hide the left and right vignettes.
                 GameObject rightBoarder = WorldCampaign.instance.worldEx.GetChild("2DMap").GetChild("BorderRight");
                 GameObject leftBoarder = WorldCampaign.instance.worldEx.GetChild("2DMap").GetChild("BorderLeft");
-            
-                leftBoarder.TryDestroy();
-                rightBoarder.TryDestroy();
+
+                rightBoarder.transform.eulerAngles = new(rightBoarder.transform.eulerAngles.x, 90, rightBoarder.transform.eulerAngles.z);
+                rightBoarder.transform.SetScale(new(1f, 1f, 0.01f));
+                rightBoarder.transform.localPosition = new(-7.6125f, 1f, 0f);
+
+                leftBoarder.transform.eulerAngles = new(leftBoarder.transform.eulerAngles.x, 270, leftBoarder.transform.eulerAngles.z);
+                leftBoarder.transform.SetScale(new(1f, 1f, 0.01f));
+                leftBoarder.transform.localPosition = new(7.6125f, 1f, 0f);
+            }
+            else
+            {
+                // Expand size of vignettes to cover empty space to either side
+                GameObject rightBoarder = WorldCampaign.instance.worldEx.GetChild("2DMap").GetChild("BorderRight");
+                GameObject leftBoarder = WorldCampaign.instance.worldEx.GetChild("2DMap").GetChild("BorderLeft");
+
+                rightBoarder.transform.eulerAngles = new(rightBoarder.transform.eulerAngles.x, 270, rightBoarder.transform.eulerAngles.z);
+                rightBoarder.transform.SetScale(new(1f, 1f, 0.01f));
+                rightBoarder.transform.localPosition = new(-4.95f, 1f, 0f);
+
+                leftBoarder.transform.eulerAngles = new(leftBoarder.transform.eulerAngles.x, 90, leftBoarder.transform.eulerAngles.z);
+                leftBoarder.transform.SetScale(new(1f, 1f, 0.01f));
+                leftBoarder.transform.localPosition = new(4.95f, 1f, 0f);
             }
 
             if (Config.Param("taf_hide_submarine_managment_buttons", 0) == 1 && !HasDestroyedSubmarineButton)
